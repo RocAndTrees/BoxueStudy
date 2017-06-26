5c9bcf13d4474242fab599b99877564306f553a4
linxiaopeng <359539865@qq.com>
2017-06-26 11:07:12 +0800

    no message

--- demo/P3-Stopwatch/P3-Stopwatch/ViewController.swift
+++ demo/P3-Stopwatch/P3-Stopwatch/ViewController.swift
@@ -9,17 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    /// 复位／计次
+    @IBOutlet weak var leftButton: UIButton!
+    /// 启动／停止
+    @IBOutlet weak var rightButton: UIButton!
+    /// time 显示
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+
+extension ViewController{
+    
+    
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier: String = "lapCell"
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        
+        if let labelNum = cell.viewWithTag(11) as? UILabel {
+//            labelNum.text = "Lap \(laps.count - (indexPath as NSIndexPath).row)"
+        }
+        if let labelTimer = cell.viewWithTag(12) as? UILabel {
+//            labelTimer.text = laps[laps.count - (indexPath as NSIndexPath).row - 1]
+        }
+        
+        return cell
+    }
+}
