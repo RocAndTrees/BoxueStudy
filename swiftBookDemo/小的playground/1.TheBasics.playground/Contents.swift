5feba79ee473f9c0540673b69d565054ddbfff7a
linxiaopeng <cjdesyue@126.com>
2017-12-14 16:28:45 +0800

    feat(bookDemo): *

---
+++ swiftBookDemo/小的playground/1.TheBasics.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+
+/* Integer Conversion*/
+
+let twoThousan: UInt16 = 2_000
+let one: UInt8 = 1
+let twoThousandAndOne = twoThousan + UInt16(one)
+
+/* Interger and Floating-Point Conversion*/
+
+let three = 3
+let pointOneFourOneFiveNine = 0.14159
+let pi = Double(three) + pointOneFourOneFiveNine
+
+/* Type Aliases*/
+
+typealias AudioSample = UInt16
+var maxAmplitudeFound = AudioSample.min
+
+
