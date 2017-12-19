318a3e5eac98e9771e52164171d5d1db5d81b2c7
linxiaopeng <cjdesyue@126.com>
2017-12-19 17:32:20 +0800

    feat(dispatch): add test dispatch Demo (swift, oc)

---
+++ dispatch/Dispatch_OC_Test/Dispatch_OC_Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dispatch_OC_Test
+//
+//  Created by pxl on 2017/12/19.
+//  Copyright © 2017年 yunbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
